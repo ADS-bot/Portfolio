4e94e12efbf4f49ccbed0fbfa342d91a99ae87ac
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-06-06 18:01:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,8 +21,6 @@ if(navClose){
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
-/*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () =>{
@@ -42,12 +40,55 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form');
+const contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => {
+  e.preventDefault();
+  
+  // serviceID - templateID - #form - publicKey
+  emailjs.sendForm('service_1uf7u6l', 'template_fari503', '#contact-form', '40tdCW2jCa7nOoDKE')
+    .then(() => {
+      contactMessage.textContent = 'Message Sent Successfully ✅';
+      setTimeout(() => {
+        contactMessage.textContent = '';
+      }, 5000);
+      contactForm.reset();
+    }, () => {
+      contactMessage.textContent = 'Message not sent (service error) ❌';
+    });
+};
+
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
